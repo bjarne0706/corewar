b8355743b21bd3c75d6a7853a86ef3c3b30c0cc9
Home <home@Dont-Panic.local>
2019-05-15 12:46:33 +0300

    add more

--- asm/asm_main.c
+++ asm/asm_main.c
@@ -29,18 +29,26 @@ int				main(int ac, char **av)
 {
 	char		*filename;
 	char 		**arr;
+	char		*line;
 
 	g_files = (t_files *)ft_memalloc(sizeof(t_files));
-	if (ac != 2)
+	if (ac != 2) {
 		error("Error: incorrect number of files");
+	}
 	arr = ft_strsplit(av[1], '.');
-	if (ft_strcmp(arr[1], "s") != 0)
+	if (ft_strcmp(arr[1], "s") != 0) {
 		error("Error: incorrect file extension");
+	}
 	g_files->f_fd = open(av[1], O_RDONLY);
 	filename = get_name(av[1]);
 	g_files->s_fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0666);
 	write_header();
-	write_player_name();
+	while (get_next_line(g_files->f_fd, &line) > 0)
+	{
+		if (ft_strstr(line, ".name"))
+			write_player_name(line);
+		free(line);
+	}
 }
 
 void			write_header()
@@ -54,9 +62,30 @@ void			write_header()
 	write(g_files->s_fd, tmp, 4);
 }
 
-void				write_player_name()
+void				write_player_name(char *line)
 {
+	int 	brack_flag;
+	char 	*name;
+	char 	*tmp_name;
 
+	name = (char *)ft_memalloc(PROG_NAME_LENGTH);
+	tmp_name = (char *)ft_memalloc(PROG_NAME_LENGTH);
+	brack_flag = 0;
+	while (brack_flag != 2)
+	{
+		if (ft_strchr(line, '"'))
+			brack_flag++;
+		if (ft_strrchr(ft_strchr(line, '"'), '"'))
+			brack_flag++;
+		tmp_name = ft_strjoin(tmp_name, line);
+		get_next_line(g_files->f_fd, &line);
+		if (brack_flag != 2)
+			tmp_name = ft_strjoin(tmp_name, line);
+//		printf("%s\n", line);
+	}
+		printf("%s\n", tmp_name);
+	name = ft_memcpy(name, &tmp_name, 128);
+	write(g_files->s_fd, name, 128);
 }
 
 unsigned int	reverse_byte(unsigned int num)
