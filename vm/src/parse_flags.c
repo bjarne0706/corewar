6b01bf3437c86d08113c16c660302217cfb9d8b2
Litavets <litavets@gmail.com>
2019-05-18 19:56:31 +0300

    Completes flag parsing for now. cor file validation in progress

---
+++ vm/src/parse_flags.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_flags.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dstepane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/18 12:59:18 by dstepane          #+#    #+#             */
+/*   Updated: 2019/05/18 12:59:21 by dstepane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/vm.h"
+
+void			flag_d(char *av[], int ac, int *i, t_vm *v)
+{
+	if (!v->options[0] && (*i + 1 < ac) && ft_isint(av[*i + 1]))
+	{
+		v->dump_cycles[0] = ft_atoi(av[*i + 1]);
+		if (v->dump_cycles[0] < 0)
+			v->dump_cycles[0] = -1;
+		else
+			v->options[0] = 'd';
+		*i += 1;
+	}
+	else
+		print_usage();
+	
+}
+
+void			flag_s(char *av[], int ac, int *i, t_vm *v)
+{
+	if (!v->options[1] && (*i + 1 < ac) && ft_isint(av[*i + 1]))
+	{
+		v->dump_cycles[1] = ft_atoi(av[*i + 1]);
+		if (v->dump_cycles[1] < 0)
+			v->dump_cycles[1] = -1;
+		else
+			v->options[1] = 's';
+		*i += 1;
+	}
+	else
+		print_usage();
+	
+}
+
+void			flag_v(t_vm *v)
+{
+	if (!v->options[2])
+		v->options[2] = 'v';
+	else
+		print_usage();
+}
+
+void			parse_args(int ac, char *av[], t_vm *v)
+{
+	int		i;
+
+	i = 1;
+	while (i < ac)
+	{
+		if (!ft_strcmp(av[i], "-d"))
+			flag_d(av, ac, &i, v);
+		else if (!ft_strcmp(av[i], "-s"))
+			flag_s(av, ac, &i, v);
+		else if (!ft_strcmp(av[i], "-v"))
+			flag_v(v);
+		else if (!ft_strcmp(av[i], "-n"))
+			flag_n(av, ac, &i, v);
+		else if (cor_filename(av[i]))
+			parse_champ(v, av[i]);
+		else
+			print_usage();
+		i++;
+	}
+}
--- vm/src/vm_main.c
+++ vm/src/vm_main.c
@@ -36,66 +36,14 @@ Usage: ./corewar [-d N -s N -v N | -b --stealth | -n --stealth] [-a] <champion1.
 void			print_usage(void)
 {
 	ft_printf("{b}{yellow}{u}USAGE:{0}\n");
-	ft_printf("	{yellow}./corewar [-d N -s N] [-v] [-n N]");
-	ft_printf(" champ1.cor [-n N] champ2.cor <...>{-}\n");
+	ft_printf("	{yellow}./corewar [-d N -s N] [-v] <[-n N]");
+	ft_printf(" champ1.cor> <[-n N] champ2.cor> <...>{-}\n");
 	ft_printf("\n{u}{b}FLAGS:\n{0}");
 	ft_printf("	{b}-d N{0}	: Dumps memory after N cycles then exits\n");
 	ft_printf("	{b}-s N{0}	: Runs N cycles, dumps memory, pauses, then repeats\n");
 	ft_printf("	{b}-v{0}	: Visual mode\n");
 	ft_printf("	{b}-n N{0}	: Specifies player's number\n");
 	exit(0);
-}
-
-void			flag_d(char *av[], int ac, int *i, t_vm *v)
-{
-	if (!v->options[0] && (*i + 1 < ac) && ft_isint(av[*i + 1]))
-	{
-		v->dump_cycles[0] = ft_atoi(av[*i + 1]);
-		if (v->dump_cycles[0] < 0)
-			v->dump_cycles[0] = -1;
-		else
-			v->options[0] = 'd';
-		*i += 1;
-	}
-	else
-		print_usage();
-	
-}
-
-void			flag_s(char *av[], int ac, int *i, t_vm *v)
-{
-	if (!v->options[1] && (*i + 1 < ac) && ft_isint(av[*i + 1]))
-	{
-		v->dump_cycles[1] = ft_atoi(av[*i + 1]);
-		if (v->dump_cycles[1] < 0)
-			v->dump_cycles[1] = -1;
-		else
-			v->options[1] = 's';
-		*i += 1;
-	}
-	else
-		print_usage();
-	
-}
-
-void			parse_args(int ac, char *av[], t_vm *v)
-{
-	int		i;
-
-	i = 1;
-	while (i < ac)
-	{
-		if (!ft_strcmp(av[i], "-d"))
-			flag_d(av, ac, &i, v);
-		else if (!ft_strcmp(av[i], "-s"))
-			flag_s(av, ac, &i, v);
-/*		else if (!ft_strcmp(av[i], "-v"))
-			flag_v();
-		else if (!ft_strcmp(av[i], "-n"))
-			flag_n();
-*/		
-		i++;
-	}
 }
 
 int				main(int ac, char *av[])
